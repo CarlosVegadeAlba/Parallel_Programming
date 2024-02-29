d8f3a3e76b51092e8841045c73667dcca8a22eaf
CarlosVegadeAlba <carlos.vegad@estudiante.uam.es>
2024-02-29 22:48:27 +0100

    Remove comments and Prepare Parallel Sorting

--- Assigment_1/radix_sequential (backup).c
+++ Assigment_1/radix_sequential (backup).c
@@ -16,9 +16,9 @@ void getStringFromNumber(unsigned long long number, char * numberInBits);
 
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        // At least 4 arguments
-        printf("Usage: %s number_of_elements number_of_bits number_of_threads\n", argv[0]);
+    if (argc != 3) {
+        // At least 3 arguments
+        printf("Usage: %s number_of_elements number_of_bits\n", argv[0]);
         return -1;
     }
 
@@ -39,8 +39,7 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    int number_of_threads = atoi(argv[3]);
-    printf("Radix Parallel Sorting with %d elements, %d bits, %d threads\n", number_of_elements, number_of_bits, number_of_threads);
+    printf("Radix Sequential Sorting with %d elements and %d bits\n", number_of_elements, number_of_bits);
 
     unsigned long long* rawArray = malloc(number_of_elements * sizeof(unsigned long long));
     unsigned long long* outputArray = malloc(number_of_elements * sizeof(unsigned long long));
@@ -56,19 +55,30 @@ int main(int argc, char *argv[]) {
     int numIterations;
 
     // Random seed
-    srand((unsigned) time(NULL));
+    /* srand((unsigned) time(NULL));
     int randomValue = rand();
-    init_genrand64(randomValue);
+    init_genrand64(randomValue); */
     
+    // Create the list 
+    /* printf("List of elements: \n\t["); */
     for (int i=0; i<number_of_elements-1; i++){
         randomNum = genrand64_int64(); 
-        rawArray[i] = randomNum;
+        rawArray[i] = randomNum; 
+        /* printf("%llu, ", rawArray[i]); */
     }
     randomNum = genrand64_int64();
     rawArray[number_of_elements-1] = randomNum;
+    /* printf("%llu]\n", rawArray[number_of_elements-1]); */
+
+    /* printf("Raw Array: \n[");
+    for(int j=0; j< number_of_elements-1; j++){
+        printf("%llu, ", rawArray[j]);
+    }
+    printf("%llu]\n", rawArray[number_of_elements-1]); */
 
     // Get the max number
     maxNum = max(rawArray, number_of_elements);
+    /* printf("Maximum: %llu\n", maxNum); */
     
     numIterations = getNumberOfIterationsFromMax(maxNum, number_of_bits);
     
@@ -76,6 +86,7 @@ int main(int argc, char *argv[]) {
         numKeys = numKeys*2;
     }
 
+    /* printf("If there are %d bits there %llu possible numbers\n", number_of_bits, numKeys); */
     unsigned long long* countKeys = malloc(numKeys * sizeof(unsigned long long));
     if(!countKeys){
         fprintf(stderr, "Memory allocation failed\n");
@@ -86,44 +97,90 @@ int main(int argc, char *argv[]) {
     }
 
     unsigned int bits = (1U << number_of_bits) - 1;
+    /* char numberInBits[MAXNBITS+1]="\0";
+    getStringFromNumber(bits, numberInBits);
+    printf("AUXILIAR VALUE FOR AND OPERATION %s\n", numberInBits); */
 
     // Start timing
     clock_t start = clock();
     for(int i=0; i<numIterations; i++){
+       /*  printf("\nIteration %d\n", i); */
 
         // Reset the key array
         for(int j=0; j<numKeys; j++){
             countKeys[j]=0;
         }
+        /* printf("Hola1?\n"); */
 
         // Fill the key array
         for(int j=0; j<number_of_elements; j++){
             aux2 = rawArray[j];
             aux1 = (aux2 >> (i*number_of_bits)) & bits; //aux1 has the bits we are studing in this iteration
+
+            /* char numberInBits1[MAXNBITS+1]="\0";
+            char numberInBits2[MAXNBITS+1]="\0";
+            getStringFromNumber(aux2, numberInBits1);
+            getStringFromNumber(aux1, numberInBits2); 
+            printf("\t-The number %llu: %s\n\thas a value of %llu\n", rawArray[j], numberInBits1, aux1); */
             countKeys[aux1]++;
         }
+        /* printf("Hola2?\n"); */
+        
+        // Show Count Array
+        /* printf("Raw Count Array\n");
+        printf("[");
+        for(int j=0; j<numKeys; j++){
+            printf("%llu,", countKeys[j]);
+        }
+        printf("]\n");  */
 
         // Do cumulative sum
         for(int j=1; j<numKeys; j++){
             countKeys[j]= countKeys[j-1] + countKeys[j];
         }
+        /* printf("Hola3?\n"); */
+
+        // Show Count Array
+        /* printf("Accumulative sum Count Array\n");
+        printf("[");
+        for(int j=0; j<numKeys; j++){
+            printf("%llu,", countKeys[j]);
+        }
+        printf("]\n"); */
 
         // Fill the output array
         for(int j=number_of_elements-1; j>=0; j--){
+            /* printf("\tHola3.5?\n"); */
             aux2 = rawArray[j];
             aux1 = (aux2 >> (i*number_of_bits)) & bits; //aux1 has the bits we are studing in this 
             countKeys[aux1]--;
+            /* printf("%llu\n", countKeys[aux1]);
+            printf("\t\tThe value %llu has to go to the position %llu\n", rawArray[j], countKeys[aux1]); */
             outputArray[countKeys[aux1]] = rawArray[j];
             
         }
+        /* printf("Hola4?\n"); */
 
         // update rawArray
         for(int j=0; j< number_of_elements; j++){
             rawArray[j] = outputArray[j];
         }
+        /* printf("Hola5?\n"); */
+        // Result of this iteration
+        /* printf("Result of this iteration: ");
+        for(int j=0; j< number_of_elements; j++){
+            printf("%llu, ", rawArray[j]);
+        }
+        printf("\n"); */
     }
     // End timing
     clock_t end = clock();
+
+    /* printf("Final Sorted Array: \n[");
+    for(int j=0; j< number_of_elements-1; j++){
+        printf("%llu, ", rawArray[j]);
+    }
+    printf("%llu]\n", rawArray[number_of_elements-1]); */
 
     // Check if the array is sorted well
     for(int j=0; j< number_of_elements-1; j++){
@@ -173,7 +230,9 @@ unsigned long long max(unsigned long long *list, int lenght){
 int getNumberOfIterationsFromMax(unsigned long long maxNum, int numBits){
 
     int numberOfBitsMaximum = countBits(maxNum);
+    /* printf("Number of bitsMaximum: %d, Number of Bits per number: %d\n", numberOfBitsMaximum, numBits); */
     int numberOfIterations = ((numberOfBitsMaximum-1) / numBits) +1;
+    /* printf("Number of iterations: %d\n", numberOfIterations); */
 
     return numberOfIterations;
 }
@@ -190,9 +249,11 @@ int countBits(unsigned long long number)
     int aux, currentBit;
     char numberInBits[MAXNBITS+1]="\0";
 
+    /* printf("Loop:\t"); */
     for (int i=0; i<MAXNBITS; i++){
         currentBit = number & 1;
         number >>= 1; //Shift the number 1 bit
+        /* printf("%d", currentBit); */
         numberInBits[i]= '0'+currentBit;
     }
     int i = MAXNBITS-1;
@@ -201,6 +262,8 @@ int countBits(unsigned long long number)
             break;
         }
     }
+    /* printf("\nString:\t%s\n", numberInBits);
+    printf("The maximum bit set to 1 is at position %d\n", i+1);  */
 
     return i+1;
 }
