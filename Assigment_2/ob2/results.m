58ac48d081f12d7288ab53a9a44dee63de00fd08
CarlosVegadeAlba <carlos.vegad@estudiante.uam.es>
2024-03-15 15:19:01 +0100

    First Commit Assigment 2

---
+++ Assigment_2/ob2/results.m
@@ -0,0 +1,5 @@
+x = [1 10 20 30 40 50 ];
+name = {'/export/data/fredrikm/inf236/smallGraphs/test1' ,'/export/data/fredrikm/inf236/smallGraphs/test2' ,'/export/data/fredrikm/inf236/smallGraphs/test3' ,'/export/data/fredrikm/inf236/smallGraphs/test' };
+n = [10 10 10 4 ];
+nz = [9 13 17 4 ];
+TimeSequentialBFS = [0.000005 0.000002 0.000002 0.000001 ];
